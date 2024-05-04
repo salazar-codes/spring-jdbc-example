9d7d5daf0b63aa3a10c08b7d9cf13988dfb24b8f
Jimmy Salazar <jimmyr.salazar@gmail.com>
2024-05-04 01:20:47 -0500

    feat: add default pool validation

--- src/main/java/com/jimmysalazar/springjdbc/SpringJdbcApplication.java
+++ src/main/java/com/jimmysalazar/springjdbc/SpringJdbcApplication.java
@@ -1,13 +1,26 @@
 package com.jimmysalazar.springjdbc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SpringJdbcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringJdbcApplication.class, args);
-	}
+	public static final Logger log = LoggerFactory.getLogger(SpringJdbcApplication.class);
 
+	public static void main(String[] args) {
+
+		// Al iniciar la app de Spring, puedo almacenar la ejecución que me dará paso al contexto
+		// esto me permitirá acceder a los beans/objetos administrados por Spring
+		ConfigurableApplicationContext context = SpringApplication.run(SpringJdbcApplication.class, args);
+		DataSource dataSource = context.getBean(DataSource.class);
+
+		// Comprobamos que el Pool de conecciones por default es HikariCp
+		log.info("Datasource implementation {}",dataSource.getClass().getName());
+	}
 }
